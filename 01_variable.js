a0bbd69fd44970e975c44363f213073ad14309e6
poojaraut2618 <poojaraut2618@gmail.com>
2022-12-08 23:44:26 +0530

    Javascript session
    variable declaration assignment and practice code

---
+++ 01_variable.js
@@ -0,0 +1,9 @@
+var myName;  //variablr declaration
+myName= "pooja raut"; //variable initialization
+console.log(myName);
+
+var myFrndName = "Sachin"; //variable declaration and initialization at the same time
+console.log(myFrndName);
+
+myFrndName="mitiksha"; //variable update
+console.log(myFrndName);
---
+++ 01_variable_assign.js
@@ -0,0 +1,20 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName="Pooja Raut";
+console.log(myFullName);
+
+myFullName="Pooja Sachin Raut";
+console.log(myFullName);
+
+var myLove="I Love Only JS";
+console.log(myLove);
+
+var firstName="Pooja";
+var lastName="Raut";
+var age="29";
+var address="Manjari Bk, Pune ";
+var pin_code="412307";
+var gender="Female";
+var is_married="Yes";
+console.log(firstName, lastName, age, address, pin_code, gender, is_married);
