b08669b1e286da799402e449643d0c5d2a91b857
poojaraut2618 <poojaraut2618@gmail.com>
2022-12-11 23:14:47 +0530

    Practice variables and functions
    Practice variables and functions

--- 01_variable.js
+++ 01_variable.js
@@ -7,3 +7,26 @@ console.log(myFrndName);
 
 myFrndName="mitiksha"; //variable update
 console.log(myFrndName);
+
+console.log("======Learning undefined=======");
+var country;
+console.log(country);
+country="India";
+console.log(country);
+
+var companyName="";
+console.log(typeof companyName);
+
+var age=29;
+var myAgeType= typeof age;
+console.log(myAgeType);
+console.log(typeof age);
+
+
+
+ var microsoftOwner = "BillGates";
+console.log(typeof microsoftOwner);
+ microsoftOwner=45;
+console.log(typeof microsoftOwner);
+ microsoftOwner= false;
+console.log(typeof microsoftOwner);
---
+++ 02_function.js
@@ -0,0 +1,42 @@
+function display() {   //function definition
+    //Task to perform
+    console.log("My Name : Pooja");
+    console.log("My Sir Name is : Raut");
+}
+display(); //Calling function to display or function invocation
+
+// write a function to show which should log you college name
+
+function collegeName(){
+    console.log("College Name :  Modern College of Arts, Commerce & Science ");
+}
+collegeName();
+
+function showDetails(myName){
+    console.log("Details :", myName);
+}
+showDetails("Pooja Raut");
+showDetails("Mitiksha Raut");
+showDetails("Sachin Raut");
+
+function swapValues(value1, value2){
+    console.log("Before Swap :", value1, value2);
+    var temp = value1;
+    value1 = value2;
+    value2 = temp;
+    console.log("After Swap :", value1, value2);
+}
+swapValues(100, 200);
+swapValues(2, 4);
+swapValues("pooja", "mitiksha");
+swapValues("you", "me");
+
+function rajuBhaiya(paisa){
+    console.log("Going in Market");
+    console.log("Purchase Fresh Vegetables");
+    console.log("Come Back");
+    return "Bag of Vegetables";
+    return 25+25; //unreacheable code
+}
+var bagOfVegetables = rajuBhaiya(100);
+console.log(bagOfVegetables);
