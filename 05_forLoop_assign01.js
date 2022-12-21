1bb245ebb03eb8e34c7609233136a6824dd0b6c0
poojaraut2618 <poojaraut2618@gmail.com>
2022-12-22 00:12:36 +0530

    forLoop assignement
    forLoop assignement

---
+++ 05_forLoop_assign01.js
@@ -0,0 +1,38 @@
+console.log(`\n=====step 1=====`)
+for(i=5; i<=15; i++){
+    console.log(i);
+}
+
+console.log(`\n=====step 2=====`)
+for(i=50; i>=40; i=i-1){
+    console.log(i);
+}
+
+console.log(`\n=====step 3=====`)
+for(i=0; i<=15; i++){
+    if(i%2 != 0){
+        console.log(i);
+    }
+}
+
+console.log(`\n=====step 4=====`)
+for(i=0; i<=10; i++){
+    if(i%2 == 0){
+        console.log(i);
+    }
+}
+
+console.log(`\n=====step 5=====`)
+for(i=5; i<=50; i=i+5){
+    console.log(i); 
+}
+
+console.log(`\n=====step 6=====`)
+for(i=10; i<=100; i=i+10){
+    console.log(i); 
+}
+
+console.log(`\n=====step 7=====`)
+for(i=100; i>=10; i=i-10){
+    console.log(i); 
+}
