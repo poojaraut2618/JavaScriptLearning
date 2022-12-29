df7467be8db45fe7965126d834be7c7c3e99f1df
poojaraut2618 <poojaraut2618@gmail.com>
2022-12-29 18:04:34 +0530

    forLoop assignment
    Completed all steps (1 to 7) using forLoop

--- 05_forLoop_assign01.js
+++ 05_forLoop_assign01.js
@@ -9,9 +9,10 @@ for(i=50; i>=40; i=i-1){
 }
 
 console.log(`\n=====step 3=====`)
+var startNo = 1
 for(i=0; i<=15; i++){
-    if(i%2 != 0){
-        console.log(i);
+    if(i%2 != 0 ){
+        console.log(startNo);
     }
 }
 
